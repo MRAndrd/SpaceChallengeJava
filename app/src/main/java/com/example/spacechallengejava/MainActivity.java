b6cc5df14c5656de9df0e0e5304f9d0185afbf84
MRAndrd <maxxxjarudenko@gmail.com>
2021-10-03 12:07:02 +0300

    ``

--- app/src/main/java/com/example/spacechallengejava/MainActivity.java
+++ app/src/main/java/com/example/spacechallengejava/MainActivity.java
@@ -13,11 +13,9 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     TextView U1TextView;
     TextView U2TextView;
-    public int totalBudgetU1 = 0;
-    public int totalBudgetU2 = 0;
-    Simulation mainSimulation = new Simulation();
-    private Object Simulation;
-
+    Simulation mainSimulation = new Simulation(this);
+    int totalBudgetU1 = 0;
+    int totalBudgetU2 = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +23,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         U1TextView = (TextView) findViewById(R.id.U1TextView);
         U2TextView = (TextView) findViewById(R.id.U2TextView);
-        Button button = findViewById(R.id.launchButton);
-        button.setOnClickListener((View.OnClickListener) this);
     }
-// не так
-   // public void onClick(View view) {
-       //mainSimulation.getClass(Simulation);
-  //  }
+  public void runTheSimulation(View view) throws Exception {
+        //не розумію чому (mainSimulation.loadItems()) - 'loadU1(java.util.ArrayList , java.util.ArrayList )' in 'com.example.spacechallengejava.Simulation' cannot be applied to '(java.util.ArrayList )'
+      totalBudgetU1 = mainSimulation.runSimulation(mainSimulation.loadU1(mainSimulation.loadItems()));
+      totalBudgetU2 = mainSimulation.runSimulation(mainSimulation.loadU2(mainSimulation.loadItems()));
+      U1TextView.setText(totalBudgetU1 + "millions");
+      U2TextView.setText(totalBudgetU2 + "millions");
+  }
 }
